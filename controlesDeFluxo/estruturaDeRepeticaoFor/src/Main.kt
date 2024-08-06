d1389e7b3dfba353a04ebf7c5a78eefe315a7a20
DevFelipreis <lfreis.contato@gmail.com>
2024-08-06 15:19:54 -0300

    commit

--- controlesDeFluxo/estruturaDeRepeticaoFor/src/Main.kt
+++ controlesDeFluxo/estruturaDeRepeticaoFor/src/Main.kt
@@ -1,3 +1,26 @@
 fun main() {
-    println("Hello World!")
+
+var contagem:Int=0
+
+    println("\n")
+   for (i in 1.. 100 step  2){
+        println("${i}")
+   }
+
+    println("\n")
+    for (i in 20 downTo 0 step 4){
+        println(i)
+    }
+
+    println("\n")
+    while (contagem<100){
+        contagem+=4
+        println("$contagem")
+    }
+
+    val string = "Criação de Aplicativo Android"
+    println("\n")
+    for (i in  string){
+        println("$i")
+    }
 }
