1dcadcc0dfb348cd432e5819e209cc209bc57e7a
DevFelipreis <lfreis.contato@gmail.com>
2024-08-06 14:09:58 -0300

    commit

---
+++ controlesDeFluxo/tiposVariaveis/src/Main.kt
@@ -0,0 +1,25 @@
+fun main() {
+    var x:Int = 3000
+    val y:Int = 900
+    var a: Float = 2.6f
+    var b: Double = 2.6
+    var c: Long = 49949845985889465
+    var d: Char = 'f'
+    var e: String = "Luiz Felipe"
+    var f: Short = 2
+    var g: Byte = 0b101
+    var h: Byte = 0xf
+    var i: UInt = 12u
+
+    println(x)
+    println(y)
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+    println(i)
+}
