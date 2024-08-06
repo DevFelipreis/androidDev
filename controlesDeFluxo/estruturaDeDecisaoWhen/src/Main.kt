533b24a259269cc745582b3ad78ed8c6080117a4
DevFelipreis <lfreis.contato@gmail.com>
2024-08-06 15:01:15 -0300

    commit

--- controlesDeFluxo/estruturaDeDecisaoWhen/src/Main.kt
+++ controlesDeFluxo/estruturaDeDecisaoWhen/src/Main.kt
@@ -1,3 +1,13 @@
 fun main() {
-    println("Hello World!")
+    println("Digite seu cargo:")
+    var cargo:String = readln()
+
+    when(cargo){
+        "Diretor"-> println("$cargo -> Salário R$9000")
+        "Gerente"-> println("$cargo -> Salário R$3000")
+        "Encarregado"-> println("$cargo -> Salário R$2300")
+        "Conferente"-> println("$cargo -> Salário R$1700")
+        "Separador"-> println("$cargo -> Salário R$1412")
+        else -> println("$cargo -> Cargo inválido")
+    }
 }
