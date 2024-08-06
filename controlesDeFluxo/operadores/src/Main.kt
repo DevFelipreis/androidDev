d38d3b5943ad022c029b582f22e7099484bdf09d
DevFelipreis <lfreis.contato@gmail.com>
2024-08-06 14:42:38 -0300

    commit

---
+++ controlesDeFluxo/operadores/src/Main.kt
@@ -0,0 +1,37 @@
+fun main() {
+
+/*
+     //Operadores lógicos
+     && -> E/END
+     || -> OU/OR
+     ! -> NEGAÇÃO/NOT
+
+    //Operadores aritméticos/matemáticos
+    + -> ADIÇÃO
+    - -> SUBTRAÇÃO
+    * -> MULTIPLICAÇÃO
+    / -> DIVISÃO
+    ^ -> POTENCIAÇÃO
+    % -> RESTO DA DIVISÃO/MOD
+
+    //operadores comparatívos
+    > -> MAIOR QUE
+    < -> MENOR QUE
+    >= -> MAIOR OU IGUAL QUE
+    < -> MENOR OU IGUAL
+    = -> ATRIBUIÇÃO DE VALOR
+    == -> IGUAL(NÃO COMPARA O TIPO PRIMITIVO
+    === -> IGUAL(COMPARA O TIPO PRIMITIVO
+    != -> DIFERENTE (NÃO COMPARA O TIPO PRIMITIVO
+    !== -> DIFERENTE (COMPARA O TIPO PRIMITIVO
+
+ */
+
+    val number = 34
+
+    if(number%2==0){
+        println("O número $number é par")
+    } else{
+        println("O número $number é ímpar")
+    }
+}
