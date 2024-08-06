6c90f9142baf2f267d3a04f0018b38eff5de58c1
DevFelipreis <lfreis.contato@gmail.com>
2024-08-06 14:29:38 -0300

    commit

--- controlesDeFluxo/variaveisMutaveisImutaveisTiposBoolean/src/Main.kt
+++ controlesDeFluxo/variaveisMutaveisImutaveisTiposBoolean/src/Main.kt
@@ -1,3 +1,19 @@
 fun main() {
-    println("Hello World!")
+    var inteiro :Int = 19
+    val string: String = "Total" // imutável
+    var boolean: Boolean = true
+
+    println("Inteiro:$inteiro, String:$string e Boolean:$boolean")
+
+    inteiro = 20
+    boolean = !true
+
+    println("Inteiro:$inteiro, String:$string e Boolean:$boolean")
+
+    var doubleInte = inteiro.toDouble()
+    doubleInte = 99.99
+    boolean = false
+
+    println("Double:$doubleInte, String:$string e Boolean:$boolean")
+
 }
