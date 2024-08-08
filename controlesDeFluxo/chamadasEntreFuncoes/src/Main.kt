fc9cc238cc0e84d6aa8065cb4615d6ba5670a86a
DevFelipreis <lfreis.contato@gmail.com>
2024-08-08 15:06:30 -0300

    commit

---
+++ controlesDeFluxo/chamadasEntreFuncoes/src/Main.kt
@@ -0,0 +1,27 @@
+fun main() {
+    casa()
+}
+
+fun casa(){
+println("Cheguei em casa")
+    quarto()
+}
+
+fun quarto(){
+println("Entrando no quarto")
+    guardaRoupas()
+}
+
+fun guardaRoupas(){
+    println("Abri o guarda-roupas")
+    sapatos()
+}
+
+fun sapatos(){
+println("peguei os sapatos no guarda-roupas")
+    cadarço()
+}
+
+fun cadarço(){
+println("Coloquei os cadarços nos sapatos")
+}
