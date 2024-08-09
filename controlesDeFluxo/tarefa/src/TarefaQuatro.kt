908dc59f87eb26b726b4ec94995a3b517de79bad
DevFelipreis <lfreis.contato@gmail.com>
2024-08-09 15:55:02 -0300

    commit

---
+++ controlesDeFluxo/tarefa/src/TarefaQuatro.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val names= arrayOf("Felipe", "João", "Pedro", "Silva", "Maria", "Eduardo", "Sula", "Dede", "Esther", "Sasá")
+    var index = 1
+
+    for (i in names){
+        println("Nome $index: $i")
+        index++
+    }
+}
--- controlesDeFluxo/tarefa/src/TarefaTres.kt
+++ controlesDeFluxo/tarefa/src/TarefaTres.kt
@@ -1,17 +1,17 @@
 fun main() {
     val number = 17
     var index = 2
-    var ePrimo = true
+    var ehPrimo = true
 
     while (index <= number / 2) {
         if (number % index == 0) {
-            ePrimo = false
+            ehPrimo = false
             break
         }
         index++
     }
 
-    if (ePrimo && number > 1) {
+    if (ehPrimo && number > 1) {
         println("$number é um número primo")
     } else {
         println("$number não é um número primo")
